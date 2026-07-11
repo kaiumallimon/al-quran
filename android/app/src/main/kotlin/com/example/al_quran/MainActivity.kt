3f220a31fe1c96d7e5762b5465ff2f9a20712341
Kaium Al Limon <limon.3cs@gmail.com>
2026-07-11 09:44:31 +0600

    refactor: change MainActivity to extend AudioServiceActivity instead of FlutterActivity

--- android/app/src/main/kotlin/com/example/al_quran/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/al_quran/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.al_quran
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : AudioServiceActivity()
